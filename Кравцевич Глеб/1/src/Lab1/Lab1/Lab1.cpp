048ae8e5cc949296df4d676dfa5870493ed969dd
KGleb <krawcevitsch@gmail.com>
2021-09-03 16:56:10 +0300

    First commit

---
+++ Кравцевич Глеб/1/src/Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "address.h"
+
+using namespace std;
+
+int main()
+{
+    Address* address_empty = new Address();
+    address_empty->display();
+
+    address_empty->set_name("Name");
+
+    Address* address_valid = new Address("Name", "Street", 15);
+    address_valid->display();
+
+    Address* address_copy = new Address(*address_valid);
+    address_copy->display();
+
+    delete address_empty;
+    delete address_valid;
+    delete address_copy;
+
+    getchar();
+    return 0;
+}
