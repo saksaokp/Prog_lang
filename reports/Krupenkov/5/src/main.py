dd958fcd3d7f29a876db01ef2be341ca0aeda73a
FalseR20 <kno3.2002@gmail.com>
2021-09-13 11:49:01 +0300

    feat: code for lab5 is ready

--- reports/Krupenkov/5/src/main.py
+++ reports/Krupenkov/5/src/main.py
@@ -1,8 +1,20 @@
+from pprint import pprint
 from typing import List, Optional
 
 
-def reading():
-    with open('files/students.csv', 'r', encoding='utf-8') as file:
+def printing(text=None, header=None, students=None):
+    if text:
+        print(text)
+    if header:
+        print('Header:')
+        pprint(header)
+    if students:
+        print('Students:')
+        pprint(students)
+
+
+def load(filename):
+    with open('files/' + filename, 'r', encoding='utf-8') as file:
         students: List[List[Optional[str, int]]] = []
         for line in file.read().split('\n'):
             students.append(line.split(';'))
@@ -11,16 +23,18 @@ def reading():
         student[0] = int(student[0])
         student[2] = int(student[2])
     students.sort(key=lambda x: x[1])
+    printing(f'Loaded from {filename}:', header, students)
     return header, students
 
 
 def decrease(students):
     for student in students:
         student[2] -= 1
-    # print(students)
+    printing('Decreased:', None, students)
 
 
-def save(header, students):
+def save(filename, header, students):
+    # filename = 'new_students.csv'
     to_save: str = ''
     row_sep = ''
     for student in [header] + students:
@@ -30,14 +44,32 @@ def save(header, students):
         for i in student:
             to_save += val_sep + str(i)
             val_sep = ';'
-    with open('files/new_students.csv', 'w', encoding='utf-8') as file:
+    with open('files/' + filename, 'w', encoding='utf-8') as file:
         file.write(to_save)
+    printing(f'Saved to {filename}:', header, students)
+
+
+def menu(header, students):
+    while True:
+        print('\nMenu:')
+        print('1: Уменьшить возраст всех студентов на 1')
+        print('2: Сохранить')
+        print('3: Загрузить')
+        print('0: Выход из программы')
+        inp = input()
+        if inp == '1':
+            decrease(students)
+        elif inp == '2':
+            save(input('Введите название файла для сохранения: '), header, students)
+        elif inp == '3':
+            load(input('Введите название файла для загрузки: '))
+        elif inp == '0':
+            break
+
 
 def main():
-
-    header, students = reading()
-    decrease(students)
-    save(header, students)
+    header, students = load('students.csv')
+    menu(header, students)
 
 
 if __name__ == '__main__':
