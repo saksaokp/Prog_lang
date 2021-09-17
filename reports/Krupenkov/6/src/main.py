b4410a7b6f22e5aa3158b6916dc82a6f0953edac
FalseR20 <kno3.2002@gmail.com>
2021-09-17 21:32:41 +0300

    lab6 ...

--- reports/Krupenkov/6/src/main.py
+++ reports/Krupenkov/6/src/main.py
@@ -1,4 +1,20 @@
 # Вариант 9 задания 1.18, 2.13
 
 
-print('Hello World')
+class Bus:
+    __count = 0
+    __name: str
+    __speed: int
+
+    def __init__(self, name='nameless', speed=0) -> None:
+        self.__count += 1
+        self.__name = name
+        self.__speed = speed
+
+    def __str__(self) -> str:
+        return f'Class Bus (name: {self.__name}, speed: {self.__speed})'
+
+
+bus = Bus()
+print(bus)
+
