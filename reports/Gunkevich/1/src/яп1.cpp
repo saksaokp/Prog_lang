4e43757a552c6cce43de8dcd4231a6c4557360e6
Hirasavskiy <leagoo2002@gmail.com>
2021-09-18 12:51:50 +0300

    add lab1

---
+++ reports/Gunkevich/1/src/яп1.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <iostream>
+#include "class.h"
+
+int main()
+{
+    setlocale(0, "Russian");
+    resorces* S[3];
+    S[0] = new resorces("Иван", 5, 1);
+    S[1] = new resorces();
+    S[2] = new resorces("Виталий", 1, 1);
+
+    S[1]->set("Дмитрий", 3, 0);
+
+    resorces* s1 = S[1];
+
+    for (int i = 0; i < 3; i++) S[i]->print(); //использование указателя на компоненту - функцию
+    s1->print(); //использование указателя на объект
+    delete[] S;
+    return 0;
+}
