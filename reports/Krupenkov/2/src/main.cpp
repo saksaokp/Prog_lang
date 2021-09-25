135f3f6f939c55e002f97cf949ef9cfbad31d5a4
FalseR20 <kno3.2002@gmail.com>
2021-09-25 20:19:50 +0300

    edit(lab2): code in main.cpp

--- reports/Krupenkov/2/src/main.cpp
+++ reports/Krupenkov/2/src/main.cpp
@@ -16,9 +16,11 @@ public:
         cout << "Created SaleItem \"" << name << "\"\n";
     }
 
-    virtual void print() {
-        cout << "SaleItem  name: \"" << name << "\", price: " << price << "\n";
+    ~SaleItem() {
+        cout << "Deleted SaleItem \"" << name << "\"\n";
     }
+
+    virtual void print() = 0;
 
     virtual void add() final {
         SaleItem** temp = items;
@@ -39,27 +41,73 @@ public:
 
 
 class Toy : SaleItem {
+protected:
+    int min_age;
 
+public:
+    Toy(const char *name, float price, int min_age) : SaleItem(name, price), min_age(min_age) {
+        cout << "Created Toy \"" << name << "\"\n";
+    }
+
+    ~Toy() {
+        cout << "Deleted Toy \"" << name << "\"\n";
+    }
+
+    void print() override {
+        cout << "Toy with name: \"" << name << "\", price: " << price << ", min_age: " << min_age << "\n";
+    }
 };
 
 
-class Product : SaleItem {
+class Product : protected SaleItem {
+protected:
+    bool isAlcoholic;
 
+public:
+    Product(const char *name, float price, int isAlcoholic) : SaleItem(name, price), isAlcoholic(isAlcoholic) {
+        cout << "Created Product \"" << name << "\"\n";
+    }
+
+    ~Product() {
+        cout << "Deleted Product \"" << name << "\"\n";
+    }
+
+    void print() override {
+        cout << "Product with name: \"" << name << "\", price: " << price << ", isAlcoholic: " << isAlcoholic << "\n";
+    }
 };
 
 
-class MilkProduct : Product {
+class MilkProduct : Product{
+protected:
+    float fatness;
 
+public:
+    MilkProduct(const char *name, float price, int isAlcoholic, float fatness) :
+    Product(name, price, isAlcoholic), fatness(fatness) {
+        cout << "Created MilkProduct \"" << name << "\"\n";
+    }
+
+    ~MilkProduct() {
+        cout << "Deleted MilkProduct \"" << name << "\"\n";
+    }
+
+    void print() override {
+        cout << "MilkProduct with name: \"" << name << "\", price: " << price
+        << ", isAlcoholic: " << isAlcoholic << ", fatness " << fatness << "\n";
+    }
 };
+
 
 int SaleItem::count = 0;
 SaleItem **SaleItem::items = nullptr;
 
 
 int main() {
-    SaleItem phone("phone", 700);
-    SaleItem pen("pen", 1);
-    SaleItem pencil("pencil", 0.80);
-    SaleItem::showItems();
+    Toy pony("AppleJack", 7, 0);
+    Product beer("beer", 1.59, true);
+    Product crisps("crisps", 3.47, false);
+    MilkProduct milk("milk", 1.59, false, 3.6);
 
+    SaleItem::showItems();
 }
