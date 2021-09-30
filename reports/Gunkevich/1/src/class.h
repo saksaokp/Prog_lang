97cf338574f02ac67a2e8bd502ea2bced9d01d35
Hirasavskiy <leagoo2002@gmail.com>
2021-09-30 17:13:03 +0300

    new library

--- reports/Gunkevich/1/src/class.h
+++ reports/Gunkevich/1/src/class.h
@@ -9,9 +9,9 @@ private:
 public:
     resorces();
     resorces(const char* n, int a, int g);
-    resorces(const resorces& s);//РєРѕРїРёСЂСѓСЋС‰РёР№ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
+    resorces(const resorces& s);//копирующий конструктор
     ~resorces();
-    void set(const char* n, int a, int g);//СѓСЃС‚Р°РЅРѕРІРєР° РїРѕР»РµР№ РґР°РЅРЅС‹С…
-    void print();//РїСЂРѕСЃРјРѕС‚СЂ РїРѕР»РµР№ РґР°РЅРЅС‹С…
+    void set(const char* n, int a, int g);//установка полей данных
+    void print();//просмотр полей данных
 };
 #endif
