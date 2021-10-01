af0792eef85dff34049748ed4afdfb977098be91
FalseR20 <kno3.2002@gmail.com>
2021-10-01 18:22:08 +0300

    lab3: fix error

--- reports/Krupenkov/3/src/main.cpp
+++ reports/Krupenkov/3/src/main.cpp
@@ -12,17 +12,17 @@ using namespace std;
 class Figure {
 protected:
     float a, b;
-    static list<Figure*> figures;
+    static list<Figure *> figures;
 
-    void add() {
+public:
+    Figure(float a, float b) : a(a), b(b) {
+        cout << "Created abstract Figure: a = " << a << ", b = " << b << "\n";
         figures.push_back(this);
     }
 
-public:
+    ~Figure() {
+        cout << "Deleted abstract Figure: a = " << a << ", b = " << b << "\n";
 
-    Figure(float a, float b) : a(a), b(b) {
-        cout << "Created abstract Figure: a=" << a << ", b=" << b << "\n";
-        add();
     }
 
     virtual float s() = 0;
@@ -31,11 +31,16 @@ public:
 
     virtual void print() = 0;
 
-//    static void show() {
-//        for (auto & figure : figures) {
-//            cout << figure->a;
-//        }
-//    }
+    virtual void printSP() final {
+        cout << "S = " << s() << ", P = " << p() << "\n";
+    }
+
+    static void show() {
+        for (auto &figure : figures) {
+            figure->print();
+            figure->printSP();
+        }
+    }
 };
 
 
@@ -45,11 +50,14 @@ protected:
 
 public:
     Rhombus(float a, float alpha) : Figure(a, a), alpha(alpha) {
-        cout << "Created Rhombus: a=b=" << a << ", alpha=" << alpha << "\n";
+        cout << "Created Rhombus: a = b = " << a << ", alpha = " << alpha << "\n";
+    }
+
+    ~Rhombus() {
+        cout << "Deleted Rhombus: a = b = " << a << ", alpha = " << alpha << " rad\n";
     }
 
     float s() override {
-        cout << sin(alpha) << endl;
         return a * a * sin(alpha);
     }
 
@@ -58,14 +66,17 @@ public:
     }
 
     void print() override {
-        cout << "Rhombus: a=b=" << a << ", alpha=" << alpha << "\n";
+        cout << "Rhombus: a = b = " << a << ", alpha = " << alpha << " rad\n";
     }
 };
+
+
+list<Figure *> Figure::figures;
+
 
 int main() {
     const float PI = 3.141592653589793;
     Rhombus r(2, PI / 6);
-    cout << r.s() << ' ' << r.p();
-//    Figure::show();
+    Figure::show();
 
 }
