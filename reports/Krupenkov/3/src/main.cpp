55576ce417855542f417f975a6b7fe6928948394
FalseR20 <kno3.2002@gmail.com>
2021-10-02 19:48:52 +0300

    edit lab6 something

--- reports/Krupenkov/3/src/main.cpp
+++ reports/Krupenkov/3/src/main.cpp
@@ -17,22 +17,17 @@ protected:
 
 public:
     Figure(float a, float b) : a(a), b(b) {
-        cout << "Created abstract Figure: a = " << a << ", b = " << b << "\n";
         figures.push_back(this);
     }
 
-    ~Figure() {
-        cout << "Deleted abstract Figure: a = " << a << ", b = " << b << "\n";
 
-    }
+    virtual float s() const = 0;
 
-    virtual float s() = 0;
+    virtual float p() const = 0;
 
-    virtual float p() = 0;
+    virtual void print() const = 0;
 
-    virtual void print() = 0;
-
-    virtual void printSP() final {
+    virtual void printSP() const final {
         cout << "S = " << s() << ", P = " << p() << "\n";
     }
 
@@ -41,16 +36,22 @@ public:
             figure->print();
             cout << "  ";
             figure->printSP();
+            cout << "\n";
         }
     }
 
-    bool operator==(Figure &other) {
+    bool operator==(const Figure &other) {
         return (typeid(*this) == typeid(other)) && (a == other.a) && (b == other.b) && (s() == other.s());
     }
 
-    bool operator!=(Figure &other) {
+    bool operator!=(const Figure &other) {
         return !(*this == other);
     }
+
+    Figure &operator=(const Figure &other) {
+        return *this;
+    }
+
 };
 
 
@@ -59,23 +60,17 @@ protected:
     float alpha;
 
 public:
-    Parallelepiped(float a, float b, float alpha) : Figure(a, b), alpha(alpha) {
-        cout << "Created Parallelepiped: a = " << a << ", b = " << b << ", alpha = " << alpha << "\n";
-    }
+    Parallelepiped(float a, float b, float alpha) : Figure(a, b), alpha(alpha) {}
 
-    ~Parallelepiped() {
-        cout << "Deleted Parallelepiped: a = " << a << ", b = " << b << ", alpha = " << alpha << "\n";
-    }
-
-    float s() override {
+    float s() const override {
         return a * b * sin(alpha);
     }
 
-    float p() override {
+    float p() const override {
         return (a + b) * 2;
     }
 
-    void print() override {
+    void print() const override {
         cout << "Parallelepiped: a = " << a << ", b = " << b << ", alpha = " << alpha << "\n";
     }
 };
@@ -83,15 +78,9 @@ public:
 
 class Rhombus : public Parallelepiped {
 public:
-    Rhombus(float a, float alpha) : Parallelepiped(a, a, alpha) {
-        cout << "Created Rhombus: a = b = " << a << ", alpha = " << alpha << "\n";
-    }
+    Rhombus(float a, float alpha) : Parallelepiped(a, a, alpha) {}
 
-    ~Rhombus() {
-        cout << "Deleted Rhombus: a = b = " << a << ", alpha = " << alpha << " rad\n";
-    }
-
-    void print() override {
+    void print() const override {
         cout << "Rhombus: a = b = " << a << ", alpha = " << alpha << " rad\n";
     }
 };
@@ -99,40 +88,76 @@ public:
 
 class Ellipse : public Figure {
 public:
-    Ellipse(float a, float b) : Figure(a, b) {
-        // a, b - полуоси эллипса
-        cout << "Created Ellipse: a = " << a << ", b = " << b << "\n";
-    }
+    Ellipse(float a, float b) : Figure(a, b) {}
 
-    ~Ellipse() {
-        cout << "Deleted Ellipse: a = " << a << ", b = " << b << "\n";
-    }
-
-    float s() override {
+    float s() const override {
         return PI * a * b;
     }
 
-    float p() override {
-        // return 2 * PI * sqrt((a * a + b * b) / 2);
+    float p() const override {
         return float(4 * (PI * a * b + pow((a - b), 2)) / (a + b));
     }
 
-    void print() override {
+    void print() const override {
         cout << "Ellipse: a = " << a << ", b = " << b << "\n";
     }
 };
 
+
+class FiguresList {
+    FiguresList *next;
+    FiguresList *last;
+    Figure *self;
+
+public:
+    FiguresList() : next(nullptr), last(nullptr), self(nullptr) {}
+
+    void add(Figure *figure) {
+        if (last == nullptr) {
+            self = figure;
+            last = this;
+        } else {
+            last->next = new FiguresList;
+            last = last->next;
+            last->self = figure;
+        }
+    }
+
+    void print() const {
+        if (last != nullptr) {
+            self->print();
+            const FiguresList *ptr = this;
+            while (ptr->next != nullptr) {
+                ptr = ptr->next;
+                ptr->self->print();
+            }
+        }
+    }
+
+    Figure &operator[](int x) {
+        FiguresList *ptr = this;
+        for (int i = 0; i < x; i++) {
+            ptr = ptr->next;
+        }
+        return *ptr->self;
+    }
+};
 
 list<Figure *> Figure::figures;
 
 
 int main() {
     Parallelepiped pip(3, 6, PI / 3);
-    Parallelepiped pip2(3, 6, PI / 2);
     Rhombus sus(2, PI / 6);
     Ellipse ell(2, 8);
-    cout << '\n';
+
     Figure::show();
-    cout << '\n';
+
+    FiguresList figures;
+    figures.add(&pip);
+    figures.add(&sus);
+    figures.add(&ell);
+    figures[0] = figures[1];
+    figures.print();
 
 }
