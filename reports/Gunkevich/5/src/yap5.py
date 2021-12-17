833638e6a439f74da54c1ebab2907577d8e2ad6f
Hirasavskiy <leagoo2002@gmail.com>
2021-12-18 01:28:38 +0300

    usless update for lab5

--- reports/Gunkevich/5/src/yap5.py
+++ reports/Gunkevich/5/src/yap5.py
@@ -1,47 +1,75 @@
 import os, csv
+
+
 def task_1():
    files = os.listdir("D:/folder")
-   print ('Folder содержит файлы: ', files)
-   print ('Количество файлов: ', len(files))
+   print('Folder содержит файлы: ', files)
+   print('Количество файлов: ', len(files))
+
 
 def task_2():
     students = read()
-    students.sort(key = sort_num)
+    students.sort(key=sort_num)
     print("Сортировка по номеру группы:")
     output(students)
     writer(students)
     students.clear
 
+
 def writer(arr):
-    f = open ("students.csv", "w", newline = "")
+    f = open("students.csv", "w", newline="")
     writer = csv.writer(f)
     for i in arr:
         line = [';'.join(i)]
         writer.writerow(line)
     f.close()
 
+
 def output(arr):
     print("№ ФИО \t Возраст Группа")
     for i in arr:
         for j in range(len(i)):
             if i[j] == "№": break
-            print(i[j], end = " ")
+            print(i[j], end=" ")
         print()
     print()
 
+
 def read():
-    f = open ("students.csv", "r")
+    f = open("students.csv", "r")
     reader = csv.reader(f)
     arr = []
     for i in reader:
         arr.append(str(i)[2:len(i) - 3].split(';'))
     f.close()
-    print ("Исходные данные: ", arr)
-    output (arr)
+    print("Исходные данные: ", arr)
+    output(arr)
     return arr
-    
+
+
+def writer5(arr):
+    f = open("students2.csv", "w", newline="")
+    writer = csv.writer(f)
+    for i in arr:
+        line = [';'.join(i)]
+        writer.writerow(line)
+    f.close()
+
+
+def read5():
+    f = open("students2.csv", "r")
+    reader = csv.reader(f)
+    arr = []
+    for i in reader:
+        arr.append(str(i)[2:len(i) - 3].split(';'))
+    f.close()
+    print("Исходные данные: ", arr)
+    output(arr)
+    return arr
+
 def sort_num(i):
     return i[3]
+
 
 def task_3():
     students = read()
@@ -49,16 +77,24 @@ def task_3():
     for i in range(len(students)):
         if students[i][3] == num_group:
             students[i][2] = str(int(students[i][2]) + 1)
-    print ("Новый список:\n ", students)
-    output (students)
-    writer (students)
+    print("Новый список:\n ", students)
+    output(students)
+    writer(students)
     students.clear
+
+
+def task_5():
+    students = read()
+    writer5(students)
+
 
 def main():
     while True:
         print("Задание 1 - 1")
         print("Задание 2 - 2")
         print("Задание 3 - 3")
+        print("Считать из отдельного файла - 4")
+        print("Записать в отдельный файл - 5")
         a = int(input("Введите номер задания или 0 для выхода:"))
         if a == 1:
             task_1()
@@ -66,7 +102,12 @@ def main():
             task_2()
         elif a == 3:
             task_3()
+        elif a == 5:
+            task_5()
+        elif a == 4:
+            read5()
         elif a == 0: break
         else: print("НЕПРАВИЛЬНЫЙ ВВОД!")
+
 
 main()
